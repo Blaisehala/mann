e41d8677f4d8bd476debc5b86a0bf49b4fc79a92
Blaise Hala <blaisehala@gmail.com>
2022-06-13 14:02:01 +0300

    build:adds profile URL

--- vAwwards/urls.py
+++ vAwwards/urls.py
@@ -26,9 +26,11 @@ urlpatterns = [
 
     path ('register/', mann_views.register, name='register' ),
 
-    path ('login/', auth_views.LoginView.as_view(), name='login' ),
+    path ('profile/', mann_views.profile, name='profile' ),
 
-    path ('login/', auth_views.LogoutView.as_view(), name='logout' ),
+    path ('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login' ),
+
+    path ('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout' ),
 
 
     path ('', include('mann.urls')),
