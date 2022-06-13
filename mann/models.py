1b8120c6971e65d7bc3fa29191cddbe25b6150d9
Blaise Hala <blaisehala@gmail.com>
2022-06-13 11:34:05 +0300

    CI:adds the class Profile

--- mann/models.py
+++ mann/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-class Posts (models.Model):
+class Profile (models.Model):
+  user= models.OneToOneField(User, on_delete=models.CASCADE)
+  image= models.ImageField(default='default.jpg')
+  
+  
+  
+  
+class Project (models.Model):
   image = models.ForeignKey
   title = models.CharField(max_length=100, null=True, blank=True)
-  # post  = HTMLField(max_length=)
+
   description = models.TextField(max_length=300, null=True)
+  user= models.ForeignKey(User, on_delete=models.CASCADE, default="" )
