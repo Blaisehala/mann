e886a724213813f726c1d78a38aad88e641fd477
Blaise Hala <blaisehala@gmail.com>
2022-06-15 11:20:44 +0300

    CI:alterations

--- vAwwards/settings.py
+++ vAwwards/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'bootstrap4',
+    'ckeditor',
+    'ckeditor_uploader',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +128,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_CONFIGS = {
+    'default': { # You can change this based on your requirements.
+        'width': 'auto',
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
