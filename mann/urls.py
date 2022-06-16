1dc04fa34775f633c5983fe5dd368b539bf83f40
Blaise Hala <blaisehala@gmail.com>
2022-06-16 04:21:04 +0300

    fix:fixing landing page url

--- mann/urls.py
+++ mann/urls.py
@@ -6,13 +6,13 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.landing, name='mann-landing'),
-    path('home/', views.home, name='mann-home'),
+    # path('', views.landing, name='mann-landing'),
+    path('', views.home, name='mann-home'),
     path('about/', views.about, name='mann-about'),
     path ('newpost/',views.new_post, name='newpost'),
     path ('rate/<int:id>',views.rate, name='rate'),
-    path ('home/api/profile/',views.ProfileList.as_view(), name=''),
-    path ('home/api/project/',views.ProjectList.as_view(), name=''),
+    path ('api/profile/',views.ProfileList.as_view(), name=''),
+    path ('api/project/',views.ProjectList.as_view(), name=''),
     
        
 ]
