ddef7b7097702b83fdc589968dab912aa4c7bb97
Blaise Hala <blaisehala@gmail.com>
2022-06-16 12:29:53 +0300

    test

--- mann/tests.py
+++ mann/tests.py
@@ -4,6 +4,7 @@ from .models import Profile,Project
 # Create your tests here.
 class ProfileTestClass(TestCase):
   def setUp(self):
+    
     self.jiji = Profile(image='http')
   def test_instance(self):
     self.assertTrue(isinstance(self.jiji,Profile))
