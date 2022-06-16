c06458dc89017276c763592c18c06da3e6d5a9d5
Blaise Hala <blaisehala@gmail.com>
2022-06-16 07:32:28 +0300

    test

--- vAwwards/settings.py
+++ vAwwards/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-MODE = config('MODE', default='dev')
+MODE = config('MODE')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY ='django-insecure-rg)m$jb+nhyolsiarp)%6^941@z^3p9@%8*hlfj4nyxwa2bn!8'
 DEBUG = config('DEBUG', default=False, cast=bool)
