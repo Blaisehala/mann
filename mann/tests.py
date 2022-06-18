4cdfe574d8907b0645d3e2581619f97f92058249
Blaise Hala <blaisehala@gmail.com>
2022-06-18 16:44:51 +0300

    chore:running tests

--- mann/tests.py
+++ mann/tests.py
@@ -1,9 +1,30 @@
+from turtle import title
 from django.test import TestCase
-from .models import Profile,Project
+from .models import Profile,Project, Rating
 
 # Create your tests here.
 class ProfileTestClass(TestCase):
   def setUp(self):
-    self.jiji = Profile(image='http')
+    self.jiji = Profile(image='http//')
   def test_instance(self):
     self.assertTrue(isinstance(self.jiji,Profile))
+
+
+
+
+
+
+class ProjectleTestClass(TestCase):
+  def setUp(self):
+    self.first = Project(image='http',title='hey', description='hello')
+  def test_instance(self):
+    self.assertTrue(isinstance(self.first,Project))
+
+
+class RatingleTestClass(TestCase):
+  def setUp(self):
+    self.two = Rating(design='1', usability='1', content='1')
+  def test_instance(self):
+    self.assertTrue(isinstance(self.two,Rating))
+
+
